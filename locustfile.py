6a38b4267cf320236e01f5f44b39f7be6580f49d
ChaithanyaN1109 <88227102+ChaithanyaN1109@users.noreply.github.com>
2021-10-19 12:21:54 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,8 @@
 from locust import HttpUser,TaskSet,task
 import json
+import locust.stats
+locust.stats.CSV_STATS_INTERVAL_SEC = 5 # default is 1 second
+locust.stats.CSV_STATS_FLUSH_INTERVAL_SEC = 60 # Determines how often the data is flushed to disk, default is 10 seconds
 
 
 class Test_1(HttpUser):
